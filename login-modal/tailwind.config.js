c774563ed63d8b5db1f8dde4fcb37e7db0dae2e2
Juan Escalada <juanescalada175@gmail.com>
2022-12-26 10:03:43 -0800

    Add login modal miniproject

--- login-modal/tailwind.config.js
+++ login-modal/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
   },
   plugins: [],
 };
