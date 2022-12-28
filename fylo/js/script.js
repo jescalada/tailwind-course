ea0f52ce4db329fdff400271c1d07cbbc7daf5a1
Juan Escalada <juanescalada175@gmail.com>
2022-12-28 12:40:57 -0800

    Add dark mode toggler and localstorage

---
+++ fylo/js/script.js
@@ -0,0 +1,43 @@
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+function checkIfDarkMode() {
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    // Show Light Icon
+    themeToggleLightIcon.classList.remove("hidden");
+    themeToggleDarkIcon.classList.add("hidden");
+  } else {
+    // Show Dark Icon
+    themeToggleDarkIcon.classList.remove("hidden");
+    themeToggleLightIcon.classList.add("hidden");
+  }
+}
+
+// Add listener to dark-mode toggle button
+themeToggleBtn.addEventListener("click", () => {
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
