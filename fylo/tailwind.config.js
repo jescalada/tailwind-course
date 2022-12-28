8485bfd61a30bf9d73ce8d428cd9aac35b6e7383
Juan Escalada <juanescalada175@gmail.com>
2022-12-28 13:23:54 -0800

    Add Hero section

--- fylo/tailwind.config.js
+++ fylo/tailwind.config.js
@@ -20,8 +20,8 @@ module.exports = {
       backgroundImage: () => ({
         "logo-dark-mode": "url('../images/logo-dark-mode.svg')",
         "logo-light-mode": "url('../images/logo-light-mode.svg')",
-        "curvy-dark-mode": "url('../images/curvy-dark-mode.svg')",
-        "curvy-light-mode": "url('../images/curvy-light-mode.svg')",
+        "curvy-dark-mode": "url('../images/bg-curvy-dark-mode.svg')",
+        "curvy-light-mode": "url('../images/bg-curvy-light-mode.svg')",
       }),
     },
   },
